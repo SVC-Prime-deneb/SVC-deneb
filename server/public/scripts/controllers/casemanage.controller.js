422e913efbf723c99e3de3703f1df51edb7c40bc
Emi Chen <emimbchen@gmail.com>
2017-12-13 14:05:53 -0600

    adding ng-clicks

--- server/public/scripts/controllers/casemanage.controller.js
+++ server/public/scripts/controllers/casemanage.controller.js
@@ -3,4 +3,22 @@ myApp.controller('CaseController', function (FormService) {
     var vm = this;
     vm.formService = FormService;
     vm.formObject = FormService.formObject;
+
+    vm.showMa = function($event, id){
+
+    }
+
+    vm.showLa = function ($event, id) {
+
+    }
+
+    vm.showRefer = function ($event, id) {
+
+    }
+
+    vm.showRelease= function ($event, id) {
+
+    }
+
+
 });
