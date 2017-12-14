3a1e8ca1fb84c24f21f26acfe9d51f45bbcda713
Emi Chen <emimbchen@gmail.com>
2017-12-14 14:25:18 -0600

    header styling

--- server/public/scripts/controllers/ma.controller.js
+++ server/public/scripts/controllers/ma.controller.js
@@ -1,4 +1,4 @@
-myApp.controller('MaController', function (FormService, $http) {
+myApp.controller('MaController', function (FormService, $http, $mdDialog) {
     console.log('MaController created');
     var vm = this;
     vm.formService = FormService;
@@ -7,16 +7,15 @@ myApp.controller('MaController', function (FormService, $http) {
 
     vm.victimization = [{string: 'Adult Sexual Assault', was_adult_sexual_assault: true}, {string: 'Sexual Exploitation', was_sexual_exploitation: true}, {string: 'Minor-CSA', was_minor_other: true}, {string: 'Family/Minor-CSA', was_minor_family: true}, {string: 'Other', was_other: true}];
     vm.services = ['1:1','Legal Advocacy','Support Group'];
-
     
-    vm.submitNewMa(mc.newMa) {
-        $http.put('/trip/' + type + '/' + tripId, objectTosend).then(function (response) {
-            console.log('new', type, 'sent');
+    
+    vm.submitNewMa= function(objectToSend) {
+        console.log(objectToSend);
+        $http.put('/case/update/ma/' + vm.currentFormId.currentId, objectToSend).then(function (response) {
+            console.log('new MA form sent');
             $mdDialog.hide();
-            vm.getThisTrip(tripId);
-            vm.hideEdit();
         }).catch(function (error) {
-            console.log('update not sent :(');
+            console.log('new MA form not sent');
         })
     }
 
