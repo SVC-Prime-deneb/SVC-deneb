5f9951e06faf9f19673c55aef113a294a1592933
Emi Chen <emimbchen@gmail.com>
2017-12-14 16:52:11 -0600

    moved dialog functions to service

--- server/public/scripts/services/forms.service.js
+++ server/public/scripts/services/forms.service.js
@@ -1,4 +1,4 @@
-myApp.service('FormService', function ($http, $location) {
+myApp.service('FormService', function ($http, $location, $mdDialog) {
     console.log('FormService Loaded');
     var self = this;
     self.FormObject = {};
@@ -8,9 +8,57 @@ myApp.service('FormService', function ($http, $location) {
     
     self.currentFormId = {currentId: 0};
 
+    //function to set current formId
     self.saveFormId = function(id){
         self.currentFormId.currentId = id; 
         console.log(self.currentFormId);
+    }
+
+    //show medical advocate dialog function
+    self.showMa = function (ev, id) {
+        self.saveFormId(id);
+        $mdDialog.show({
+            templateUrl: '../views/partials/maform.html',
+            controller: 'MaController as mc',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose: true,
+        })
+    }
+
+    //show legal advocate dialog function
+    self.showLa = function (ev, id) {
+        self.saveFormId(id);
+        $mdDialog.show({
+            templateUrl: '../views/partials/laiform.html',
+            controller: 'LaiController as lc',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose: true,
+        })
+    }
+
+    //show referral form dialog function
+    self.showRefer = function (ev, id) {
+        self.saveFormId(id);
+        $mdDialog.show({
+            templateUrl: '../views/partials/releaseform.html',
+            controller: 'ReleaseController as rc',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose: true,
+        })
+    }
+
+    self.showRelease = function (ev, id) {
+        self.saveFormId(id);
+        $mdDialog.show({
+            templateUrl: '../views/partials/releaseinfoform.html',
+            controller: 'ReleaseInfoController as ric',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose: true,
+        })
     }
     
 
