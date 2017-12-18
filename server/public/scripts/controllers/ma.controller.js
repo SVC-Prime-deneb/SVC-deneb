ddf4c68495c15e6a3b34a2307f0dbcb4f95f2d7f
Emi Chen <emimbchen@gmail.com>
2017-12-18 08:43:54 -0600

    worked on edit mode

--- server/public/scripts/controllers/ma.controller.js
+++ server/public/scripts/controllers/ma.controller.js
@@ -8,7 +8,6 @@ myApp.controller('MaController', function (FormService, $http, $mdDialog) {
     vm.victimization = [{string: 'Adult Sexual Assault', was_adult_sexual_assault: true}, {string: 'Sexual Exploitation', was_sexual_exploitation: true}, {string: 'Minor-CSA', was_minor_other: true}, {string: 'Family/Minor-CSA', was_minor_family: true}, {string: 'Other', was_other: true}];
     vm.services = ['1:1','Legal Advocacy','Support Group'];
     
-    
     vm.submitNewMa= function(objectToSend) {
         console.log(objectToSend);
         $http.put('/case/update/ma/' + vm.currentFormId.currentId, objectToSend).then(function (response) {
