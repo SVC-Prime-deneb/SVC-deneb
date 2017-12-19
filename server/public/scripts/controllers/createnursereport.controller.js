a1b0dcd7f7924a86425010e34d497878688f2eed
Hanna Nguyen <ha.nguyenthidan@gmail.com>
2017-12-19 15:07:05 -0600

    added nursereport table and controller

--- server/public/scripts/controllers/createnursereport.controller.js
+++ server/public/scripts/controllers/createnursereport.controller.js
@@ -2,4 +2,17 @@ myApp.controller('CreateNurseReportController', function (ReportService, $http) 
     console.log('CreateNurseReportController created');
     var vm = this;
     vm.reportService = ReportService;
+
+    vm.nurseReportHospital = "";
+    vm.nurseReportId = "";
+    vm.newNurseReport = {
+        nurse_was_adv_dispatched: false
+    };
+
+    vm.submitNurseReport = function (objectTosend) {
+        console.log(objectTosend);
+        $http.post('/report/new/nursereport', objectTosend).then(function (response) {
+        }).catch(function (err) {
+        }
+    )}
 })
